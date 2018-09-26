c4b81f4843243fef77097b810e9b2864d95ff9ab
Ariel Nunez <ingenieroariel@gmail.com>
2018-09-26 08:30:19 -0500

    Added CSV support

--- main.go
+++ main.go
@@ -3,11 +3,28 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"encoding/csv"
+  	"strings"
+  	"log"
+  	"io"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		r := csv.NewReader(strings.NewReader(in))
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+
+             for {
+  		record, err := r.Read()
+  		if err == io.EOF {
+  			break
+  		}
+  		if err != nil {
+  			log.Fatal(err)
+  		}
+  
+  		fmt.Println(record)
+      	}
 	})
 
 	http.ListenAndServe(":80", nil)
